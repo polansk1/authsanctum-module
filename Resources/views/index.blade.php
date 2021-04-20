0219f3cfa5a5f9b1e89445114d5ba75302aecd59
Pol Colomo <pol.colomo@visyon360.com>
2021-04-20 11:40:35 +0200

    delete unused files

--- Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('authsanctum::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('authsanctum.name') !!}
-    </p>
-@endsection
